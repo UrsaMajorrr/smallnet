3afdd2a0fd018246f31d5ee8e2d41c72e0b7abce
Kade Carlson <carlsonkade3232@gmail.com>
2023-09-13 19:23:34 -0400

    added gradient checks

--- smallnet/engine.py
+++ smallnet/engine.py
@@ -5,21 +5,52 @@ import matplotlib.pyplot as plt
 # value object
 class Value:
 
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
+        self.grad = 0.0
+        self._prev = set(_children)
+        self._op = _op
 
     def __repr__(self):
-        return f"data = {self.data}"
+        return f"Value(data = {self.data})"
     
     def __add__(self, other):
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, (self, other), '+')
         return out
     
     def __mul__(self, other):
-        out = Value(self.data * other.data)
+        out = Value(self.data * other.data, (self, other), '*')
         return out
     
 a = Value(2.0)
 b = Value(-3.0)
-print(a + b)
-print(a * b)
+c = Value(10.0)
+e = a * b
+d = e + c
+f = Value(-2.0)
+L = d * f
+
+def lol(): # derivative staging function
+    h = 0.001
+
+    a = Value(2.0)
+    b = Value(-3.0)
+    c = Value(10.0)
+    e = a * b
+    d = e + c
+    f = Value(-2.0)
+    L = d * f
+    L1 = L.data
+
+    a = Value(2.0 + h)
+    b = Value(-3.0)
+    c = Value(10.0)
+    e = a * b
+    d = e + c
+    f = Value(-2.0)
+    L = d * f
+    L2 = L.data
+
+    print((L2 - L1)/h)
+
+lol()
